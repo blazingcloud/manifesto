ac5358630d02f0509558f7c19896aa3995fe9fd7
Kane Baccigalupi <baccigalupi@gmail.com>
2012-09-06 17:33:54 -0700

    removing bundler experiments

--- lib/manifesto/install_hooks.rb
+++
@@ -1,7 +0,0 @@
-STDERR.puts 'got in the install hook file in manifesto directory'
-require 'rubygems'
-Gem.post_install_hooks << lambda do |inst|
-  STDERR.puts 'inside the actual hook method in manifesto'
-  require 'manifesto'
-  Manifesto.generate Dir.pwd
-end
